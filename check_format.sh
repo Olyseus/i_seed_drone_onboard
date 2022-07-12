e78b543c43c352071eb4754a9189e9cddd612e77
Ruslan Baratov <x@ruslo.dev>
2022-07-12 20:36:20 +0800

    Update format script

--- check_format.sh
+++ check_format.sh
@@ -5,7 +5,7 @@ temp_file=/tmp/__clang_format_check.txt
 echo "Temp: ${temp_file}"
 echo "Clang format: `which clang-format`"
 
-for to_check in `find . -type f \( -name '*.cpp' -or -name '*.h' -or -name '*.hpp' \) ! -path './_builds/*' ! -path './third_party/*' ! -path "${HOME}/opt" ! -path './_deps/*' ! -path './core/dji_linux_*' ! -path './core/osdk*_linux*' ! -path './core/common/osal/osal*.h' ! -path './core/manifold2/hal/hal*.h' ! -path './laser/application.*pp' ! -path './laser/dji_sdk_app_info.h'`;
+for to_check in `find . -type f \( -name '*.cpp' -or -name '*.h' -or -name '*.hpp' \) ! -path './_builds/*' ! -path './third_party/*' ! -path "${HOME}/opt" ! -path './_deps/*' ! -path './dji_linux_*' ! -path './osdk*_linux*' ! -path './common/osal/osal*.h' ! -path './manifold2/hal/hal*.h' ! -path './laser/application.*pp' ! -path './laser/dji_sdk_app_info.h'`;
 do
   echo "Checking ${to_check} ..."
   echo "To diff: ${to_check} ${temp_file}"
