cc3d243237c69f58bda525019c2f49a7dafeb6da
Ruslan Baratov <x@ruslo.dev>
2022-07-18 13:40:34 +0800

    Use server class

--- drone.cpp
+++ drone.cpp
@@ -6,91 +6,7 @@
 #include <dji_linux_helpers.hpp>  // LinuxSetup
 #include <future>
 
-template <>
-drone::api_code::api_code(const ACK::ErrorCode& error_code) {
-  if (ACK::getError(error_code) == ACK::SUCCESS) {
-    code_ = code::success;
-    return;
-  }
-
-  // Note: retry will not help
-  // error_code.info.cmd_set == OpenProtocolCMD::CMDSet::subscribe
-  // error_code.data ==
-  // OpenProtocolCMD::ErrorCode::SubscribeACK::VERSION_DOES_NOT_MATCH
-
-  BOOST_VERIFY(ACK::getError(error_code) == ACK::FAIL);
-  ACK::getErrorCodeMessage(error_code, __func__);
-}
-
-template <>
-drone::api_code::api_code(const ErrorCode::ErrorCodeType& error_code) {
-  if (error_code == ErrorCode::SysCommonErr::Success) {
-    code_ = code::success;
-    return;
-  }
-  ErrorCode::printErrorCodeMsg(error_code);
-}
-
-// https://github.com/dji-sdk/Onboard-SDK/blob/4.1.0/osdk-core/modules/inc/mop/dji_mop_define.hpp#L49
-template <>
-drone::api_code::api_code(const DJI::OSDK::MOP::MopErrCode& error_code) {
-  switch (error_code) {
-    case MOP_PASSED:
-      code_ = code::success;
-      return;
-    case MOP_FAILED:
-      spdlog::error("MOP_FAILED");
-      return;
-    case MOP_CRC:
-      spdlog::error("MOP_CRC");
-      return;
-    case MOP_PARM:
-      spdlog::error("MOP_PARM");
-      return;
-    case MOP_NOMEM:
-      spdlog::error("MOP_NOMEM");
-      throw pipeline_closed();
-    case MOP_NOTREADY:
-      spdlog::error("MOP_NOTREADY");
-      make_retry();
-      return;
-    case MOP_SEND:
-      spdlog::error("MOP_SEND");
-      return;
-    case MOP_RECV:
-      spdlog::error("MOP_RECV");
-      return;
-    case MOP_TIMEOUT:
-      spdlog::error("MOP_TIMEOUT");
-      make_retry();
-      return;
-    case MOP_RESBUSY:
-      spdlog::error("MOP_RESBUSY");
-      make_retry();
-      return;
-    case MOP_RESOCCUPIED:
-      spdlog::error("MOP_RESOCCUPIED");
-      make_retry();
-      return;
-    case MOP_CONNECTIONCLOSE:
-      spdlog::error("MOP_CONNECTIONCLOSE");
-      throw pipeline_closed();
-    case MOP_NOTIMPLEMENT:
-      spdlog::error("MOP_NOTIMPLEMENT");
-      return;
-    case MOP_UNKNOWN_ERR:
-      spdlog::error("MOP_UNKNOWN_ERR");
-      return;
-    default:
-      spdlog::error("Invalid MopErrCode: {}", error_code);
-      return;
-  }
-}
-
-void drone::api_code::make_retry() {
-  std::this_thread::sleep_for(std::chrono::milliseconds(200));
-  code_ = code::retry;
-}
+#include "server.h"
 
 drone::drone(int argc, char** argv) {
   spdlog::info("Setup for Linux");
@@ -156,16 +72,8 @@ void drone::start() {
   spdlog::info("Command bytes size: {}", command_bytes_size_);
 
   while (true) {
-    std::unique_ptr<MopServer> server{new MopServer()};
-
-    spdlog::info("Creating channel {}", channel_id);
-    api_code code{
-        server->accept(channel_id, MOP::PipelineType::RELIABLE, pipeline_)};
-    if (code.retry()) {
-      continue;
-    }
-    BOOST_VERIFY(code.success());
-    BOOST_VERIFY(pipeline_ != nullptr);
+    server server;
+    pipeline_ = server.pipeline();
 
     connection_closed_ = false;
 
@@ -184,9 +92,6 @@ void drone::start() {
     } catch (pipeline_closed&) {
       connection_closed_ = true;
     }
-
-    code = api_code{server->close(channel_id)};
-    BOOST_VERIFY(code.success());
   }
 }
 
--- drone.h
+++ drone.h
@@ -43,22 +43,6 @@ class drone {
  private:
   class pipeline_closed {};
 
-  class api_code {
-   public:
-    template <class T>
-    explicit api_code(const T&);
-
-    bool success() const { return code_ == code::success; }
-    bool retry() const { return code_ == code::retry; }
-
-   private:
-    void make_retry();
-
-    enum class code { success, failure, retry };
-
-    code code_{code::failure};
-  };
-
   void receive_data_job();
   void send_data_job();
   void send_command(interconnection::command_type::command_t);
