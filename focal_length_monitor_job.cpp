d96b09a9dce59033b62ed0f38357b761a31d4e34
Ruslan Baratov <x@ruslo.dev>
2022-11-22 11:17:21 +0800

    Focal length monitor utility

---
+++ focal_length_monitor_job.cpp
@@ -0,0 +1,56 @@
+#include <boost/assert.hpp>  // BOOST_VERIFY
+#include <spdlog/spdlog.h>
+#include <thread> // std::this_thread
+
+#include <dji_camera_manager.h>  // DjiCameraManager_Init
+#include <dji_payload_camera.h>  // DjiPayloadCamera_GetCameraHybridZoomFocalLengthOfPayload
+
+void focal_length_monitor_job() {
+  T_DjiReturnCode code{DjiCameraManager_Init()};
+  BOOST_VERIFY(code == DJI_ERROR_SYSTEM_MODULE_CODE_SUCCESS);
+
+  E_DjiCameraType camera_type;
+  constexpr E_DjiMountPosition m_pos{DJI_MOUNT_POSITION_PAYLOAD_PORT_NO1};
+  code = DjiCameraManager_GetCameraType(m_pos, &camera_type);
+  BOOST_VERIFY(code == DJI_ERROR_SYSTEM_MODULE_CODE_SUCCESS);
+  BOOST_VERIFY(camera_type == DJI_CAMERA_TYPE_H20);
+
+  T_DjiCameraManagerFirmwareVersion firmware_version;
+  code = DjiCameraManager_GetFirmwareVersion(m_pos, &firmware_version);
+  BOOST_VERIFY(code == DJI_ERROR_SYSTEM_MODULE_CODE_SUCCESS);
+
+  spdlog::info("Camera firmware version {:02d}.{:02d}.{:02d}.{:02d}",
+      firmware_version.firmware_version[0],
+      firmware_version.firmware_version[1],
+      firmware_version.firmware_version[2],
+      firmware_version.firmware_version[3]);
+
+  // https://developer.dji.com/document/b0776c88-399e-4cd7-8142-681182de3dbd
+  // At least: Matrice 300 RTK：V03.00.01.01
+  BOOST_VERIFY(firmware_version.firmware_version[0] >= 4);
+
+  while (true) {
+    const E_DjiCameraManagerFocusMode expected_focus_mode{DJI_CAMERA_MANAGER_FOCUS_MODE_AUTO};
+
+    E_DjiCameraManagerFocusMode focus_mode;
+    code = DjiCameraManager_GetFocusMode(m_pos, &focus_mode);
+    BOOST_VERIFY(code == DJI_ERROR_SYSTEM_MODULE_CODE_SUCCESS);
+
+    if (focus_mode != expected_focus_mode) {
+      spdlog::info("Changing focus mode");
+      code = DjiCameraManager_SetFocusMode(m_pos, expected_focus_mode);
+      BOOST_VERIFY(code == DJI_ERROR_SYSTEM_MODULE_CODE_SUCCESS);
+      spdlog::info("Changing focus mode: DONE");
+    }
+
+    uint16_t focal_length{0};
+    code = DjiPayloadCamera_GetCameraHybridZoomFocalLengthOfPayload(m_pos, &focal_length);
+    BOOST_VERIFY(code == DJI_ERROR_SYSTEM_MODULE_CODE_SUCCESS);
+
+    spdlog::info("Focal length: {}", focal_length);
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  }
+
+  code = DjiCameraManager_DeInit();
+  BOOST_VERIFY(code == DJI_ERROR_SYSTEM_MODULE_CODE_SUCCESS);
+}
---
+++ main_focal_length.cpp
@@ -0,0 +1,78 @@
+#include <boost/assert.hpp>
+#include <boost/filesystem.hpp>  // boost::filesystem::path
+#include <dji_linux_helpers.hpp>  // LinuxSetup
+#include <iostream>              // std::cerr
+#include <spdlog/sinks/rotating_file_sink.h>  // spdlog::sinks::rotating_file_sink_mt
+#include <spdlog/sinks/stdout_sinks.h>        // spdlog::sinks::stdout_sink_mt
+#include <spdlog/spdlog.h>
+#include <spdlog/spdlog.h>
+#include <thread>  // std::this_thread
+
+#include "application.hpp" // Application
+
+void setup_logging() {
+  auto console_sink = std::make_shared<spdlog::sinks::stdout_sink_mt>();
+  console_sink->set_level(spdlog::level::info);
+
+  const boost::filesystem::path log_path{"i_seed_drone_onboard_focal_length.log"};
+  boost::filesystem::remove(log_path);
+
+  constexpr std::size_t max_file_size{10 * 1024 * 1024};
+  constexpr std::size_t max_file_num{3};
+  constexpr bool rotate_on_open{true};
+  auto file_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>(
+      log_path.string(), max_file_size, max_file_num, rotate_on_open);
+  file_sink->set_level(spdlog::level::info);
+
+  auto logger = std::make_shared<spdlog::logger>(
+      "", spdlog::sinks_init_list({console_sink, file_sink}));
+
+  spdlog::set_default_logger(logger);
+  spdlog::set_level(spdlog::level::trace);
+
+  spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] (t:%t) [%l] %v");
+  spdlog::flush_on(spdlog::level::info);
+
+  spdlog::info("Logging to file: {}", log_path.string());
+}
+
+void focal_length_monitor_job();
+
+auto run_main(int argc, char** argv) -> int {
+  setup_logging();
+
+  try {
+    auto app{std::make_unique<Application>()};
+
+    constexpr bool enable_advanced_sensing{true};
+    auto linux_setup{std::make_unique<LinuxSetup>(argc, argv, enable_advanced_sensing)};
+
+    T_DjiOsalHandler* osal{DjiPlatform_GetOsalHandler()};
+    BOOST_VERIFY(osal);
+
+    focal_length_monitor_job();
+
+    return EXIT_SUCCESS;
+  } catch (const std::system_error& exc) {
+    spdlog::critical("System error: {} {} ({})", exc.code().category().name(),
+                     exc.code().value(), exc.what());
+  } catch (const std::exception& exc) {
+    spdlog::critical("Exception: {}", exc.what());
+  } catch (...) {
+    spdlog::critical("Unknown exception caught");
+  }
+  return EXIT_FAILURE;
+}
+
+auto main(int argc, char** argv) -> int {
+  try {
+    return run_main(argc, argv);
+  } catch (const std::system_error& exc) {
+    std::cerr << "System error " << exc.code() << " (" << exc.what() << ")\n";
+  } catch (const std::exception& exc) {
+    std::cerr << "Exception: " << exc.what() << '\n';
+  } catch (...) {
+    std::cerr << "Unknown exception caught\n";
+  }
+  return EXIT_FAILURE;
+}
