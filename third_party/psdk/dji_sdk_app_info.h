88898849582fa96176ce15fe1436c8dcd40abb0e
Ruslan Baratov <x@ruslo.dev>
2022-11-22 10:30:48 +0800

    PSDK application files

---
+++ third_party/psdk/dji_sdk_app_info.h
@@ -0,0 +1,55 @@
+/**
+ ********************************************************************
+ * @file    dji_sdk_app_info.h
+ * @brief   This is the header file for defining the structure and (exported) function prototypes.
+ *
+ * @copyright (c) 2018 DJI. All rights reserved.
+ *
+ * All information contained herein is, and remains, the property of DJI.
+ * The intellectual and technical concepts contained herein are proprietary
+ * to DJI and may be covered by U.S. and foreign patents, patents in process,
+ * and protected by trade secret or copyright law.  Dissemination of this
+ * information, including but not limited to data and other proprietary
+ * material(s) incorporated within the information, in any form, is strictly
+ * prohibited without the express written consent of DJI.
+ *
+ * If you receive this source code without DJI’s authorization, you may not
+ * further disseminate the information, and you must immediately remove the
+ * source code and notify DJI of its removal. DJI reserves the right to pursue
+ * legal actions against you for any loss(es) or damage(s) caused by your
+ * failure to do so.
+ *
+ *********************************************************************
+ */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef DJI_SDK_APP_INFO_H
+#define DJI_SDK_APP_INFO_H
+
+/* Includes ------------------------------------------------------------------*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Exported constants --------------------------------------------------------*/
+// ATTENTION: User must goto https://developer.dji.com/user/apps/#all to create your own dji sdk application, get dji sdk application
+// information then fill in the application information here.
+#define USER_APP_NAME               "laser"
+#define USER_APP_ID                 "119524"
+#define USER_APP_KEY                "19069ef112a9f1c1a71a3ebaa6a430c"
+#define USER_APP_LICENSE            "pNpU60NnzLigGnBuxRO0lPoIVaxUOpE6NugQz5uKL0WS4nkbcFnIVNpvpEp/6be09mV13BM7oyXljtrZeESDEwBrlzOwCWTteT+aT3MKTHO7O5CG/vomssnP3bJVCEsxZGQLTY4haxKguL/lPFGwdMgI9YjWP/veHYKpy0fiImN7VlXFSJ5Bmqs3FiM2DUW7TjE/IZv28VmCIgJqRYirZ06abXIK6g1S7ZmPMqmT6a2WASo7HZlF6Dt1jY4wezKVk7IPqPyisyzAWOBiGWLYXWvLXXk73bWc53Li8KozPM2NA0pFoy0cvzo6FpNmdX9V1zudaMbgvb9gmCkUmb40LA=="
+#define USER_DEVELOPER_ACCOUNT      "developers@olyseus.com"
+#define USER_BAUD_RATE              "921600"
+
+/* Exported types ------------------------------------------------------------*/
+
+/* Exported functions --------------------------------------------------------*/
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // DJI_SDK_APP_INFO_H
+/************************ (C) COPYRIGHT DJI Innovations *******END OF FILE******/
