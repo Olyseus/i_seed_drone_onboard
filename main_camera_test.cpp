81e9f484bd9bb99f04c87be88e555cb89bb7bcc1
Ruslan Baratov <x@ruslo.dev>
2023-01-09 18:44:08 +0800

    Add 'i_seed_drone_camera_test'

---
+++ main_camera_test.cpp
@@ -0,0 +1,81 @@
+#include <boost/assert.hpp>
+#include <boost/filesystem.hpp>  // boost::filesystem::path
+#include <iostream>              // std::cerr
+#include <spdlog/sinks/rotating_file_sink.h>  // spdlog::sinks::rotating_file_sink_mt
+#include <spdlog/sinks/stdout_sinks.h>        // spdlog::sinks::stdout_sink_mt
+#include <spdlog/spdlog.h>
+#include <spdlog/spdlog.h>
+#include <thread>  // std::this_thread
+
+#include "camera_psdk.h"
+
+void setup_logging() {
+  auto console_sink = std::make_shared<spdlog::sinks::stdout_sink_mt>();
+  console_sink->set_level(spdlog::level::info);
+
+  const boost::filesystem::path log_path{"i_seed_drone_camera.log"};
+  boost::filesystem::remove(log_path);
+
+  constexpr std::size_t max_file_size{10 * 1024 * 1024};
+  constexpr std::size_t max_file_num{3};
+  constexpr bool rotate_on_open{true};
+  auto file_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>(
+      log_path.string(), max_file_size, max_file_num, rotate_on_open);
+  file_sink->set_level(spdlog::level::info);
+
+  auto logger = std::make_shared<spdlog::logger>(
+      "", spdlog::sinks_init_list({console_sink, file_sink}));
+
+  spdlog::set_default_logger(logger);
+  spdlog::set_level(spdlog::level::trace);
+
+  spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] (t:%t) [%l] %v");
+  spdlog::flush_on(spdlog::level::info);
+
+  spdlog::info("Logging to file: {}", log_path.string());
+}
+
+auto run_main(int argc, char** argv) -> int {
+  setup_logging();
+
+  try {
+    BOOST_VERIFY(argc == 1);
+    BOOST_VERIFY(argv != nullptr);
+    camera_psdk c{"/var/opt/i_seed_drone_onboard/best.engine"};
+
+    gps_coordinates gps;
+    quaternion quat;
+    gimbal_data gimbal;
+
+    spdlog::info("Shoot photo");
+    c.shoot_photo(gps, quat, gimbal);
+    spdlog::info("Shoot photo: DONE");
+    while (true) {
+      spdlog::info("Check SD card");
+      c.check_sdcard();
+      std::this_thread::sleep_for(std::chrono::seconds{1});
+    }
+    return EXIT_SUCCESS;
+  } catch (const std::system_error& exc) {
+    spdlog::critical("System error: {} {} ({})", exc.code().category().name(),
+                     exc.code().value(), exc.what());
+  } catch (const std::exception& exc) {
+    spdlog::critical("Exception: {}", exc.what());
+  } catch (...) {
+    spdlog::critical("Unknown exception caught");
+  }
+  return EXIT_FAILURE;
+}
+
+auto main(int argc, char** argv) -> int {
+  try {
+    return run_main(argc, argv);
+  } catch (const std::system_error& exc) {
+    std::cerr << "System error " << exc.code() << " (" << exc.what() << ")\n";
+  } catch (const std::exception& exc) {
+    std::cerr << "Exception: " << exc.what() << '\n';
+  } catch (...) {
+    std::cerr << "Unknown exception caught\n";
+  }
+  return EXIT_FAILURE;
+}
