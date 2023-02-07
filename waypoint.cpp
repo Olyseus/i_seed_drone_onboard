04fda54d49df3f7df677ce523dc51497a8f928db
Ruslan Baratov <x@ruslo.dev>
2023-02-07 17:21:48 +0800

    Add 'waypoint' class

---
+++ waypoint.cpp
@@ -0,0 +1,42 @@
+#include "waypoint.h"
+
+#include <boost/assert.hpp> // BOOST_VERIFY
+
+waypoint::waypoint(double latitude, double longitude) : latitude_(latitude), longitude_(longitude) {
+  BOOST_VERIFY(!mission_altitude_.has_value());
+}
+
+waypoint::~waypoint() = default;
+waypoint::waypoint(waypoint&&) = default;
+
+auto waypoint::is_default_altitude() const -> bool {
+  return !mission_altitude_.has_value();
+}
+
+void waypoint::set_custom_altitude(double laser_range) {
+  BOOST_VERIFY(!is_ready());
+  BOOST_VERIFY(!mission_altitude_.has_value());
+
+  // If laser range is good and equals to 'expected_height', the new mission
+  // altitude will be the same (no need to change height):
+  //   2 * expected_height - expected_height = expected_height
+  // If laser range is zero (not possible in practice) it means we are on
+  // the ground when the initial altitude is 'expected_height'. Hence
+  // we should be 'expected_height' higher, i.e. two 'expected_height'.
+  // Bigger laser range means we need use lower mission altitude than before.
+  // Smaller laser range means we need use higher mission altitude than before.
+  mission_altitude_ = 2 * expected_height - laser_range;
+}
+
+auto waypoint::altitude() const -> double {
+  return mission_altitude_.value_or(expected_height);
+}
+
+void waypoint::set_ready() {
+  BOOST_VERIFY(!is_ready());
+  is_ready_ = true;
+}
+
+auto waypoint::is_ready() const -> bool {
+  return is_ready_;
+}
---
+++ waypoint.h
@@ -0,0 +1,37 @@
+#ifndef WAYPOINT_H_
+#define WAYPOINT_H_
+
+#include <optional>
+
+class waypoint {
+ public:
+  waypoint(double latitude, double longitude);
+  ~waypoint();
+
+  waypoint(const waypoint&) = delete;
+  waypoint(waypoint&&);
+
+  waypoint& operator=(const waypoint&) = delete;
+  waypoint& operator=(waypoint&&) = delete;
+
+  bool is_default_altitude() const;
+  void set_custom_altitude(double laser_range);
+  double altitude() const;
+
+  double lat() const { return latitude_; }
+  double lon() const { return longitude_; }
+
+  static constexpr double expected_height{15.0}; // 15m
+
+  // Forward pass is ready
+  void set_ready();
+  bool is_ready() const;
+
+ private:
+  const double latitude_;
+  const double longitude_;
+  std::optional<double> mission_altitude_;
+  bool is_ready_{false};
+};
+
+#endif // WAYPOINT_H_
