4240f263a81cb3b4c97ee8123490ed1b0514cd4a
Ruslan Baratov <x@ruslo.dev>
2023-02-07 21:28:58 +0800

    Add 'utils.h'

---
+++ utils.h
@@ -0,0 +1,8 @@
+#ifndef UTILS_H_
+#define UTILS_H_
+
+static constexpr double pi_degree{180.0};
+static constexpr double rad2deg{pi_degree / M_PI};
+static constexpr double deg2rad{M_PI / pi_degree};
+
+#endif // UTILS_H_
