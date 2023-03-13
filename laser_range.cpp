58870fd2ce08cb8b7bc37a28b1a740207c4b1388
Ruslan Baratov <x@ruslo.dev>
2023-03-13 19:56:45 +0800

    Check the time point of the received laser range value

--- laser_range.cpp
+++ laser_range.cpp
@@ -3,8 +3,10 @@
 #include <boost/assert.hpp> // BOOST_VERIFY
 #include <spdlog/spdlog.h>
 
-laser_range::laser_range() {
+#include "simulator.h"
+
 #if defined(I_SEED_DRONE_ONBOARD_SIMULATOR)
+laser_range::laser_range(simulator& s) : simulator_(s) {
   // waypoint 0, bad
   values_.push_back(13.2);
   values_.push_back(15.0);
@@ -40,18 +42,63 @@ laser_range::laser_range() {
   for (int i{0}; i < 10; ++i) {
     values_.push_back(15.0);
   }
+}
+#else
+laser_range::laser_range() = default;
 #endif
+
+laser_range::~laser_range() = default;
+
+// thread: receive_data
+void laser_range::value_received(double range) {
+  {
+    const std::lock_guard<std::mutex> lock(m_);
+
+    latest_time_point_ = clock::now();
+    BOOST_VERIFY(latest_time_point_ != invalid_time_point);
+
+    latest_value_ = range;
+    spdlog::info("Received laser range: {}", latest_value_);
+  }
+  condition_variable_.notify_one();
 }
 
+// thread: action
 auto laser_range::latest() -> double {
 #if defined(I_SEED_DRONE_ONBOARD_SIMULATOR)
-  BOOST_VERIFY(!values_.empty());
-  const double result{values_.front()};
-  values_.pop_front();
-  spdlog::info("Laser range: {}", result);
-  return result;
-#else
-  BOOST_VERIFY(false); // FIXME (implement)
-  return 0.0;
+  {
+    const std::lock_guard<std::mutex> lock(m_);
+    BOOST_VERIFY(!values_.empty());
+    const double simulated_range{values_.front()};
+    values_.pop_front();
+    spdlog::info("Simulating laser range: {}", simulated_range);
+    simulator_.laser_range(simulated_range);
+  }
 #endif
+
+  while (true) {
+    {
+      const std::lock_guard<std::mutex> lock(m_);
+      if (latest_time_point_ == invalid_time_point) {
+        spdlog::info("No laser data available");
+      } else {
+        constexpr int64_t window_ms{500};
+        const auto elapsed{clock::now() - latest_time_point_};
+        namespace ch = std::chrono;
+        if (ch::duration_cast<ch::milliseconds>(elapsed).count() > window_ms) {
+          spdlog::info("Laser data is outdated");
+          latest_time_point_ == invalid_time_point;
+        } else {
+          return latest_value_;
+        }
+      }
+    }
+
+    // need to wait for a new value
+    std::unique_lock lock{m_};
+    condition_variable_.wait(lock, [this]{
+        return latest_time_point_ != invalid_time_point;
+    });
+    BOOST_VERIFY(latest_time_point_ != invalid_time_point);
+  }
 }
--- laser_range.h
+++ laser_range.h
@@ -1,18 +1,51 @@
 #ifndef LASER_RANGE_H_
 #define LASER_RANGE_H_
 
+#include <chrono>
+#include <condition_variable>
 #include <list>
+#include <mutex>
+
+#if defined(I_SEED_DRONE_ONBOARD_SIMULATOR)
+class simulator;
+#endif
 
 class laser_range {
  public:
+#if defined(I_SEED_DRONE_ONBOARD_SIMULATOR)
+  laser_range(simulator& s);
+#else
   laser_range();
+#endif
 
+  ~laser_range();
+
+  laser_range(const laser_range&) = delete;
+  laser_range(laser_range&&) = delete;
+
+  laser_range& operator=(const laser_range&) = delete;
+  laser_range& operator=(laser_range&&) = delete;
+
+  // thread: receive_data
+  void value_received(double range);
+
+  // thread: action
   double latest();
 
  private:
 #if defined(I_SEED_DRONE_ONBOARD_SIMULATOR)
+  simulator& simulator_;
   std::list<double> values_;
 #endif
+  std::mutex m_;
+  std::condition_variable condition_variable_;
+
+  using clock = std::chrono::high_resolution_clock;
+  using time_point = clock::time_point;
+  constexpr time_point invalid_time_point{time_point::min()};
+
+  time_point latest_time_point_{invalid_time_point};
+  double latest_value_{0.0};
 };
 
 #endif // LASER_RANGE_H_
