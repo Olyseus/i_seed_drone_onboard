54b5de5e3e56d4a8928d214a84f76247ba2b7391
Ruslan Baratov <x@ruslo.dev>
2023-03-31 09:49:55 +0800

    Add class 'condition_flag'

---
+++ condition_flag.cpp
@@ -0,0 +1,25 @@
+#include "condition_flag.h"
+
+#include <boost/assert.h> // BOOST_VERIFY
+
+condition_flag::condition_flag() = default;
+condition_flag::~condition_flag() = default;
+
+void condition_flag::wait() {
+  std::unique_lock lock{m_};
+  condition_.wait(lock, [this]{ return flag_; });
+  BOOST_VERIFY(flag_);
+
+  // unset the flag and release lock
+  // (we should avoid locks in Payload SDK callback)
+  flag_ = false;
+}
+
+void condition_flag::notify() {
+  {
+    std::lock_guard lock(m_);
+    BOOST_VERIFY(!flag_);
+    flag_ = true;
+  }
+  condition_.notify_one();
+}
