f33fe3c2892bf7f46604c95d84899efb577539f0
Ruslan Baratov <x@ruslo.dev>
2023-04-13 15:14:12 +0800

    Add OLYSEUS_VERIFY macro

---
+++ olyseus_verify.cpp
@@ -0,0 +1,23 @@
+#include "olyseus_verify.h"
+
+#include <spdlog/spdlog.h>
+
+namespace olyseus {
+
+[[noreturn]] void assertion_failed(const char* expr, const char* function, const char* file,
+                      uint64_t line) {
+  // FIXME (use https://en.cppreference.com/w/cpp/utility/source_location)
+  spdlog::critical("File: {}:{}, function: {}, condition: {}", file, line,
+                   function, expr);
+  throw std::runtime_error("assertion failed");
+}
+
+[[noreturn]] void assertion_failed_msg(const char* expr, const char* msg,
+                          const char* function, const char* file,
+                          uint64_t line) {
+  spdlog::critical("File: {}:{}, function: {}, condition: {}", file, line,
+                   function, expr);
+  throw std::runtime_error(msg);
+}
+
+} // namespace olyseus
