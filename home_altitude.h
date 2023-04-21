c7f9db17ddb0f37e5b9dc6282b814a6873f3b52b
Ruslan Baratov <x@ruslo.dev>
2023-04-21 20:43:48 +0800

    Document 'home_altitude' class

--- home_altitude.h
+++ home_altitude.h
@@ -4,24 +4,32 @@
 #include <optional>
 #include <vector>
 
+/// \brief Altitude of the home point
 class home_altitude {
  public:
   home_altitude();
+  ~home_altitude() = default;
 
+  /// \cond private
   home_altitude(const home_altitude&) = delete;
   home_altitude(home_altitude&&) = delete;
 
   home_altitude& operator=(const home_altitude&) = delete;
   home_altitude& operator=(home_altitude&&) = delete;
+  /// \endcond
 
-  ~home_altitude() = default;
-
+  /// \brief Mission started, ready for collecting data
   void mission_start();
 
+  /// \brief Save the home altitude and check the consistency
+  ///     of measurements
   void set_altitude(float drone_altitude, float mission_altitude,
                     float home_altitude);
+
+  /// \return Get the saved value of home altitude
   float get_home_altitude() const;
 
+  /// \brief Mission stopped, no more data expected
   void mission_stop();
 
  private:
