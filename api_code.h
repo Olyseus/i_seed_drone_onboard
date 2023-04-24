8d70c1c3257bc138ff593591ca76bfa822bd9b7e
Ruslan Baratov <x@ruslo.dev>
2023-04-24 16:51:31 +0800

    Document 'api_code' class

--- api_code.h
+++ api_code.h
@@ -3,11 +3,18 @@
 
 #include <dji_typedef.h>  // T_DjiReturnCode
 
+/// \brief Wrapper for the
+///     <a href="https://developer.dji.com/doc/payload-sdk-api-reference/en/core/dji-error.html">T_DjiReturnCode</a>
+///     from Payload SDK
 class api_code {
  public:
+  /// \throw pipeline_closed
   explicit api_code(T_DjiReturnCode code);
 
+  /// \return \b true If the API call succeeds
   bool success() const { return code_ == code::success; }
+
+  /// \return \b true If the API call fails, and request should be repeated
   bool retry() const { return code_ == code::retry; }
 
  private:
