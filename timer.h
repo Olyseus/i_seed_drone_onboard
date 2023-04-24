3a8c5137e27ca855fe49d284e64105f59f3be7de
Ruslan Baratov <x@ruslo.dev>
2023-04-24 17:06:53 +0800

    Document 'timer' class

--- timer.h
+++ timer.h
@@ -4,10 +4,13 @@
 #include <boost/assert.hpp>  // BOOST_VERIFY
 #include <chrono>
 
+/// \brief Simple timer
 class timer {
  public:
+  /// \brief Start timer
   void start() { start_ = clock::now(); }
 
+  /// \brief Show elapsed time in milliseconds
   int64_t elapsed_ms() const {
     namespace ch = std::chrono;
     BOOST_VERIFY(start_ != time_point::min());
