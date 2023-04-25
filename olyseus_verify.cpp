3b0941be8921185b276e19627999fac98828cad6
Ruslan Baratov <x@ruslo.dev>
2023-04-25 12:08:44 +0800

    Refactor OLYSEUS_VERIFY

--- olyseus_verify.cpp
+++ olyseus_verify.cpp
@@ -6,15 +6,13 @@ namespace olyseus {
 
 [[noreturn]] void assertion_failed(const char* expr, const char* function,
                                    const char* file, uint64_t line) {
-  // FIXME (use https://en.cppreference.com/w/cpp/utility/source_location)
-  spdlog::critical("File: {}:{}, function: {}, condition: {}", file, line,
-                   function, expr);
-  throw std::runtime_error("assertion failed");
+  assertion_failed_msg(expr, "Assertion failed", function, file, line);
 }
 
 [[noreturn]] void assertion_failed_msg(const char* expr, const char* msg,
                                        const char* function, const char* file,
                                        uint64_t line) {
+  // FIXME (use https://en.cppreference.com/w/cpp/utility/source_location)
   spdlog::critical("File: {}:{}, function: {}, condition: {}", file, line,
                    function, expr);
   throw std::runtime_error(msg);
