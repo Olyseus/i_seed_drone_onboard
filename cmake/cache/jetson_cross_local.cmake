aa6c584ae89dccf52ab1f162d8dd3b0b21436186
Ruslan Baratov <x@ruslo.dev>
2023-05-10 19:53:57 +0800

    Rename internal variable

--- cmake/cache/jetson_cross_local.cmake
+++ cmake/cache/jetson_cross_local.cmake
@@ -2,11 +2,11 @@ include("${CMAKE_CURRENT_LIST_DIR}/__add_cache_path.cmake")
 
 __add_cache_path(CMAKE_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/../jetson_toolchain.cmake")
 
-set(jetson_cross "$ENV{HOME}/opt/jetson_cross")
+set(__jetson_cross "$ENV{HOME}/opt/jetson_cross")
 
-__add_cache_path(CUDAToolkit_ROOT "${jetson_cross}/usr/local/cuda-10.2/bin")
-__add_cache_path(TRT_SO_DIR "${jetson_cross}/usr/lib/aarch64-linux-gnu")
-__add_cache_path(PSDK_DIR "${jetson_cross}/usr")
+__add_cache_path(CUDAToolkit_ROOT "${__jetson_cross}/usr/local/cuda-10.2/bin")
+__add_cache_path(TRT_SO_DIR "${__jetson_cross}/usr/lib/aarch64-linux-gnu")
+__add_cache_path(PSDK_DIR "${__jetson_cross}/usr")
 
 # Protobuf compiler is run on host machine, so it should be x64
 __add_cache_path(Protobuf_PROTOC_EXECUTABLE "$ENV{HOME}/opt/protobuf/bin/protoc")
