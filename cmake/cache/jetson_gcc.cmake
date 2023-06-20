56ae6ba1682fc2db7b71c7134a2e45c5224367a0
Ruslan Baratov <x@ruslo.dev>
2023-06-20 20:06:33 +0800

    Fix CMake cache

--- cmake/cache/jetson_gcc.cmake
+++ cmake/cache/jetson_gcc.cmake
@@ -11,6 +11,7 @@ __add_cache_path(GeographicLib_DIR "${__opt_dir}/geographiclib/lib/cmake/Geograp
 __add_cache_path(spdlog_DIR "${__opt_dir}/spdlog/lib/cmake/spdlog")
 __add_cache_path(CLI11_DIR "${__opt_dir}/cli11/lib/cmake/CLI11")
 __add_cache_path(GTest_DIR "${__opt_dir}/googletest/lib/cmake/GTest")
+__add_cache_path(PSDK_DIR "${__opt_dir}/psdk")
 
 set(I_SEED_DRONE_ONBOARD_DOC OFF CACHE BOOL "")
 set(I_SEED_DRONE_ONBOARD_CUDA ON CACHE BOOL "")
